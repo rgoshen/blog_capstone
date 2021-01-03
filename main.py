abf0f928f4f065cd368c71a0bc860297cd5cd01e
rgoshen <rick.goshen@gmail.com>
2021-01-03 14:01:49 -0700

    added get all blog posts

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from flask import Flask, render_template
+import requests
 
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
-    return render_template("index.html")
+    blog_url = "https://api.npoint.io/5abcca6f4e39b4955965"
+    resp = requests.get(blog_url)
+    all_posts = resp.json()
+    return render_template("index.html", posts=all_posts)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
