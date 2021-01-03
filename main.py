6fec9763b94751f61b1bbab6b9c4744d9c7c4a95
rgoshen <rick.goshen@gmail.com>
2021-01-03 14:08:00 -0700

    added footer dynamic copyright year

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 from flask import Flask, render_template
 import requests
+from datetime import datetime
 
 
 app = Flask(__name__)
 
+today = datetime.today()
+
 
 @app.route('/')
 def home():
+    """Renders the blog start page.
+    """
+
+    current_year = today.year
+
     blog_url = "https://api.npoint.io/5abcca6f4e39b4955965"
     resp = requests.get(blog_url)
     all_posts = resp.json()
-    return render_template("index.html", posts=all_posts)
+    return render_template("index.html", year=current_year, posts=all_posts)
 
 
 if __name__ == "__main__":
